52d9b5a8ae3f9612cc8009db09c9fc2588732c71
Octave Online <webmaster@octave-online.net>
2017-12-29 18:57:22 +0000

    Scripted auto-commit

--- ex1_multi.m
+++ ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.005;
-num_iters = 400;
+alpha = 0.6;
+num_iters = 10;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -154,7 +154,7 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 
-price = X_pred*theta ; % You should change this
+price = [1 X_pred]*theta ; % You should change this
 
 
 % ============================================================
